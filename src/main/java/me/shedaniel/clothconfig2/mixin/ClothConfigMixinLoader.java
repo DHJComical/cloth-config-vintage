fed468d3ee875eb440c3cd94bf4e79f86b723af3
DHJComical <dhjcomical@163.com>
2026-07-09 11:49:40 +0800

    [mixin] Register cloth config early mixins via coremod loader

---
+++ src/main/java/me/shedaniel/clothconfig2/mixin/ClothConfigMixinLoader.java
@@ -0,0 +1,46 @@
+package me.shedaniel.clothconfig2.mixin;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@IFMLLoadingPlugin.Name("ClothConfigMixinLoader")
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.SortingIndex(1001)
+public class ClothConfigMixinLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Nullable
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+    }
+
+    @Nullable
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public List<String> getMixinConfigs() {
+        return Collections.singletonList("mixins.cloth_config.json");
+    }
+}
